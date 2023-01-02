4023377a74f6e63d94be03131d2e79bb99f9e0e3
fabricio-joao <fabriciojoao41@outlook.com>
2023-01-02 06:45:28 -0300

    Inserindo tabela Departamentos Mysql

--- estudojdbc-conexao/src/cursojdbc/conexaobancosdedados/app/Programa.java
+++ estudojdbc-conexao/src/cursojdbc/conexaobancosdedados/app/Programa.java
@@ -2,18 +2,35 @@ package cursojdbc.conexaobancosdedados.app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
 
 import cursojdbc.conexaobancosdedados.ConexaoBancoDados;
+import cursojdbc.conexaobancosdedados.ExcecaoBancoDados;
 
 public class Programa {
 
 	public static void main(String[] args) throws SQLException {
-	
-		Connection conexao = ConexaoBancoDados.retornaConexao();
-		ConexaoBancoDados.fecharConexao();
+
+		Connection conexao = null;
+		PreparedStatement ps = null;
+
+		try {
+			conexao = ConexaoBancoDados.retornaConexao();
+			
+			ps = conexao.prepareStatement(
+					"create table `Departamentos`"
+				  + "(`Id` int not null, " 
+			      + "`Setores` varchar(15))"
+			      + "default charset = utf8");
+			
+			int linha = ps.executeUpdate();
+			System.out.println("Executado! Total de linhas alteradas: " + linha);
+		
+		} catch (SQLException e) {
+			throw new ExcecaoBancoDados(e.getMessage());
+		} finally {
+			ConexaoBancoDados.fecharStatement(ps);
+			ConexaoBancoDados.fecharConexao();
+		}
 	}
 }
