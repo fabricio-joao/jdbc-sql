c694846b4ba4bb685c47f637f135fdd3983aa8cd
fabricio-joao <fabriciojoao41@outlook.com>
2023-01-02 08:52:45 -0300

    Selecionando e exibindo valores de Departamentos

--- estudojdbc-conexao/src/cursojdbc/conexaobancosdedados/app/Programa.java
+++ estudojdbc-conexao/src/cursojdbc/conexaobancosdedados/app/Programa.java
@@ -2,7 +2,9 @@ package cursojdbc.conexaobancosdedados.app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -15,35 +17,25 @@ public class Programa {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 		Connection conexao = null;
-		PreparedStatement ps = null;
+		Statement st = null;
+		ResultSet rs = null;
 
 		try {
 			conexao = ConexaoBancoDados.retornaConexao();
+			st = conexao.createStatement();
+			rs = st.executeQuery("select * from Departamentos");
 			
-			ps = conexao.prepareStatement(
-					"insert into Vendedores"
-				  + "(Nome, Email, Nascimento, Salario, DepartamentosId ) " 
-			      + "values "
-			      + "(?, ?, ?, ?, ?)");
-			
-			ps.setString(1, "Maria");
-			ps.setString(2, "maria@gmail.com");
-			ps.setDate(3, new java.sql.Date(sdf.parse("08-10-1990").getTime()));
-			ps.setDouble(4, 4000);
-			ps.setInt(5, 4);
-			
-			int linha = ps.executeUpdate();
-			System.out.println("Executado! Total de linhas alteradas: " + linha);
-		
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Setores"));
+			}
 		} 
 		catch (SQLException e) {
 			throw new ExcecaoBancoDados(e.getMessage());
 		}
-		catch (ParseException e) {
-			throw new ExcecaoBancoDados(e.getMessage());
-		}
+		
 		finally {
-			ConexaoBancoDados.fecharStatement(ps);
+			ConexaoBancoDados.fecharStatement(st);
+			ConexaoBancoDados.fecharConexaoResultSet(rs);
 			ConexaoBancoDados.fecharConexao();
 		}
 	}
